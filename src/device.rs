becd576835e3d89ca5d805ae820450e6d908c1d6
philae-ael <1511446+philae-ael@users.noreply.github>
2024-07-28 19:30:48 +0200

    add support for some callback based functions

--- src/device.rs
+++ src/device.rs
@@ -1,6 +1,11 @@
-use std::ptr::null_mut;
+use std::{
+    ffi::{c_void, CStr},
+    marker::PhantomData,
+    ptr::null_mut,
+};
 
 use anyhow::{bail, Result};
+use embree4_sys::RTCError;
 
 use crate::device_error_raw;
 
@@ -58,6 +63,64 @@ impl Device {
     pub unsafe fn as_raw_handle(&self) -> embree4_sys::RTCDevice {
         self.handle
     }
+
+    pub fn register_error_callback<'scope, F: FnMut(RTCError, &str) + 'scope>(
+        &self,
+        mut callback: F,
+    ) -> ErrorCallBackScope<'scope> {
+        // adapted from https://adventures.michaelfbryan.com/posts/rust-closures-in-ffi/
+        unsafe extern "C" fn trampoline<'scope, F: FnMut(RTCError, &str) + 'scope>(
+            user_ptr: *mut ::std::os::raw::c_void,
+            code: RTCError,
+            str: *const ::std::os::raw::c_char,
+        ) {
+            let f = &mut *(user_ptr as *mut F);
+            let s = CStr::from_ptr(str);
+            (f)(code, &s.to_string_lossy());
+        }
+
+        unsafe {
+            embree4_sys::rtcSetDeviceErrorFunction(
+                self.handle,
+                Some(trampoline::<F>),
+                &mut callback as *mut _ as *mut c_void,
+            )
+        };
+
+        ErrorCallBackScope {
+            device: self.handle,
+            lifetime: Default::default(),
+        }
+    }
+    pub fn register_device_memory_monitor_callback<
+        'scope,
+        F: FnMut(isize, bool) -> bool + 'scope,
+    >(
+        &self,
+        mut callback: F,
+    ) -> MemoryMonitorCallBackScope<'scope> {
+        // adapted from https://adventures.michaelfbryan.com/posts/rust-closures-in-ffi/
+        unsafe extern "C" fn trampoline<'scope, F: FnMut(isize, bool) -> bool + 'scope>(
+            user_ptr: *mut ::std::os::raw::c_void,
+            size: isize,
+            post: bool,
+        ) -> bool {
+            let f = &mut *(user_ptr as *mut F);
+            f(size, post)
+        }
+
+        unsafe {
+            embree4_sys::rtcSetDeviceMemoryMonitorFunction(
+                self.handle,
+                Some(trampoline::<F>),
+                &mut callback as *mut _ as *mut c_void,
+            )
+        };
+        MemoryMonitorCallBackScope {
+            device: self.handle,
+            lifetime: Default::default(),
+        }
+    }
 }
 
 impl Drop for Device {
@@ -68,6 +131,31 @@ impl Drop for Device {
     }
 }
 
+pub struct ErrorCallBackScope<'scope> {
+    device: embree4_sys::RTCDevice,
+    lifetime: PhantomData<&'scope ()>,
+}
+
+impl Drop for ErrorCallBackScope<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            embree4_sys::rtcSetDeviceErrorFunction(self.device, None, null_mut());
+        }
+    }
+}
+
+pub struct MemoryMonitorCallBackScope<'scope> {
+    device: embree4_sys::RTCDevice,
+    lifetime: PhantomData<&'scope ()>,
+}
+
+impl Drop for MemoryMonitorCallBackScope<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            embree4_sys::rtcSetDeviceMemoryMonitorFunction(self.device, None, null_mut());
+        }
+    }
+}
 #[test]
 fn try_new_valid_config() {
     let ok_device = Device::try_new(Some("verbose=0"));
